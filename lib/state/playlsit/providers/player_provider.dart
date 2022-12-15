93a9f72f00b64f0098820d1cfd87c1485ee62bc2
kurbatov <kurbatxx@gmail.com>
2022-12-15 16:42:18 +0600

    fix audiocontext

--- lib/state/playlsit/providers/player_provider.dart
+++ lib/state/playlsit/providers/player_provider.dart
@@ -2,5 +2,24 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 final audioPlayerProvider = Provider<AudioPlayer>((_) {
+  final AudioContext audioContext = AudioContext(
+    iOS: AudioContextIOS(
+      defaultToSpeaker: true,
+      category: AVAudioSessionCategory.ambient,
+      options: [
+        AVAudioSessionOptions.defaultToSpeaker,
+        AVAudioSessionOptions.mixWithOthers,
+      ],
+    ),
+    android: AudioContextAndroid(
+      isSpeakerphoneOn: true,
+      stayAwake: true,
+      contentType: AndroidContentType.music, // i change this
+      usageType: AndroidUsageType.media, // i change this
+      audioFocus: AndroidAudioFocus.gain, // i change this
+    ),
+  );
+  AudioPlayer.global.setGlobalAudioContext(audioContext);
+
   return AudioPlayer();
 });
