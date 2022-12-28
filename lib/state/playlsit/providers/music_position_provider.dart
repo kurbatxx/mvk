404d0721516a4f3b630efed3832bc3d1aed82a60
kurbatov <kurbatxx@gmail.com>
2022-12-28 17:57:43 +0600

    create music position provider

--- lib/state/playlsit/providers/music_position_provider.dart
+++ lib/state/playlsit/providers/music_position_provider.dart
@@ -3,7 +3,15 @@ import 'package:mvk/state/playlsit/providers/player_provider.dart';
 
 final musicPositionProvider = FutureProvider(
   (ref) async {
+    ref.watch(tikerProvider);
+
     final player = ref.read(playerProvider);
     return await player.getCurrentPosition() ?? const Duration();
+  },
+);
+
+final tikerProvider = StreamProvider<int>(
+  (ref) {
+    return Stream.periodic(const Duration(seconds: 1), (number) => number);
   },
 );
